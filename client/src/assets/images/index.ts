d059c666b8144b4c3bdcc4a721ad32388e7580c9
Oded Masala <105584185+odedmasala@users.noreply.github.com>
2023-05-07 21:53:28 +0300

    Add files via upload

---
+++ client/src/assets/images/index.ts
@@ -0,0 +1,4 @@
+export { default as hero } from "./hero-picture 1.png";
+export { default as googlePay } from "./Frame 2.png";
+export { default as applePay } from "./Frame 1.png";
+export { default as aboutLogo } from "./about-logo@3x 2.png";
