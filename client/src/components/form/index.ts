76f20ec4db67fdcf6ce8000aa38e7fa562daa162
Oded Masala <105584185+odedmasala@users.noreply.github.com>
2023-05-13 17:50:12 +0300

    Delete client/src directory

--- client/src/components/form/index.ts
+++
@@ -1,3 +0,0 @@
-export { default as InputFieldComponent } from "./InputFieldComponent/InputFieldComponent";
-export { default as AuthForm } from "./authForm/AuthForm";
-export { default as CheckOutForm } from "./checkOutForm/CheckOutForm";
--- client/src/layouts/index.ts
+++
@@ -1,7 +0,0 @@
-export { default as ChefOfTehWeekSection } from "./chefOfTehWeek/ChefOfTehWeekSection";
-export { default as SignatureSection } from "./Signature/SignatureSection";
-export { default as DishSection } from "./DishLayout/DishSection";
-export { default as RestSection } from "./RestLayout/RestSection";
-export { default as Navbar } from "./navbar/Navbar";
-export { default as AboutSection } from "./aboutLayout/AboutSection";
-export { default as Footer } from "./footer/Footer";
